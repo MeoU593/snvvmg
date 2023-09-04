02abf6dfadf2bb595f557ec26d639f4356005dba
MeoU593 <131269308+MeoU593@users.noreply.github.com>
2023-09-04 23:04:38 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Đào Châu Anh';
+const giftUrl = 'https://www.tiktok.com/@thelastone300823/video/7272990014508764434?lang=en';
+const eventName = 'sinh nhật vui vẻ';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc cô gái của tôi luôn tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Cảm ơn vì đã ở bên anh thời gian vừa qua.Sinh nhật tuổi 20 thật vui vẻ nhé';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
